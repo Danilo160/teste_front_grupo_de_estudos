6c3a7259a6e4079f4c8c79153e761d19b618131a
Danilo Correia <danilocorreiaviana@hotmail.com>
2022-12-06 12:26:48 -0300

    Update MyGroups.js

--- src/components/pages/MyGroups.js
+++ src/components/pages/MyGroups.js
@@ -333,6 +333,7 @@ export const MyGroups = () => {
         }
         else{
           setLabel("Erro inesperado!")
+          alert("Erro inesperado pode estar associado ao tamanho PDF! Testes foram realizados com PDF de até 1.5MB")
         }
       })
       .catch(error =>{if(error){ setLabel("Falha no servidor!")}})
